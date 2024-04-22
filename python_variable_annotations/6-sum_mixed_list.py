e2b31d3376f9fcd572af0e9f8d3e9fd9a965bf6c
Remz97 <remzikeqi@icloud.com>
2024-04-22 13:25:09 +0200

    ex6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""mix list"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum of mixed lsit"""
+    return sum(mxd_lst)
